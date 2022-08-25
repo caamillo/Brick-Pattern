45c617b9d1d4cb108c13672c0736a1056889830d
camillo <diaskygotah@gmail.com>
2022-08-26 00:48:28 +0200

    paste instaed of replacing pixels

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import cv2
 
 outputW, outputH = 1920, 1080
 outputDistanceX, OutputDistanceY = 5, 5
+outputBackgroundColor = (0, 0, 0)
 
 print('Output size:', outputW, 'x', outputH)
 
@@ -23,10 +24,23 @@ patternPixels = pattern.load()
 
 # Pattern Replacing Loop
 
+def deleteTransparency(im):
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+        alpha = im.convert('RGBA').split()[-1]
+        bg = Image.new("RGBA", im.size, outputBackgroundColor + (255,))
+        bg.paste(im, mask=alpha)
+        return bg
+    else:
+        return im
+
+"""
 for y in range(outputH):
     for x in range(outputW):
         if y < patternH and x < patternW and patternPixels[x, y][3] != 0:
             output.putpixel((x, y), patternPixels[x, y])
+"""
+
+output.paste(deleteTransparency(pattern), (300, 300))
 
 # OpenCV Show
 
